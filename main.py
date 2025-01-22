a2588e228b8ed06a883375da034da40b396e8135
Mohammadhusain Masi <163997239+mohammadhusainmasi@users.noreply.github.com>
2025-01-22 14:01:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
-tweet_template = "Give me {number} tweets on {topic} in {language}"
+tweet_template = "Give me {number} tweets on {topic} in {language} "
 
 tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
 
